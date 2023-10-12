dc19a98d77ef15273e471e81cfc8623cfb9740b1
preethamnagesh8 <preethamnagesh8@preethams-mbp.acasmart.jh.edu>
2023-10-12 12:08:01 -0400

    Added send and request threads with constants.py file

---
+++ constants.py
@@ -0,0 +1,14 @@
+# Constants file
+
+PORT = 10100
+TESTING_INTERVAL = 5
+NUM_NODES = 3
+TEST_MSG = 1
+REQUEST_MSG = 2
+SOCKET_GLOBAL_TIMEOUT = 30
+PEER_ADDRESSES = [
+        {"host": "10.203.86.20", "port": 10100},
+        {"host": "10.203.86.20", "port": 10101},
+        {"host": "10.203.86.20", "port": 10102}
+        ]
+SECRET_TOKEN = "s3cr3t_t0k3n"
