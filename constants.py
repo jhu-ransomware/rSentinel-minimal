7ca2d51ef1493bc70982f61159bee6ee1236b310
preethamnagesh8 <preethamnagesh8@Preethams-MacBook-Pro.local>
2023-10-12 14:23:48 -0400

    Modified constants for Azure VMs

--- constants.py
+++ constants.py
@@ -7,8 +7,8 @@ TEST_MSG = 1
 REQUEST_MSG = 2
 SOCKET_GLOBAL_TIMEOUT = 30
 PEER_ADDRESSES = [
-        {"host": "192.168.1.100", "port": 10100},
-        {"host": "192.168.1.101", "port": 10100},
-        {"host": "192.168.1.102", "port": 10100}
+        {"host": "10.0.0.4", "port": 10100},
+        {"host": "10.0.0.5", "port": 10100},
+        {"host": "10.0.0.6", "port": 10100}
         ]
 SECRET_TOKEN = "s3cr3t_t0k3n"
